aaa9031f3d8790021b6fc58f7bccc77550c7ea6d
sebastien.lelong <sebastien.lelong@52ce04ab-a751-0410-9c8b-31cceb902f08>
2008-10-27 17:17:05 +0000

    0.1beta branch, from which will be generated release packages

---
+++ branches/jallib-0.1beta/tools/blink-an-led.cmd
@@ -0,0 +1,244 @@
+/* ------------------------------------------------------------------------ */
+/* Title: blink-an-led.cmd - Create and compile blink-an-LED samples.       */
+/*                                                                          */
+/* Author: Rob Hamerling, Copyright (c) 2008..2008, all rights reserved.    */
+/*                                                                          */
+/* Adapted-by:                                                              */
+/*                                                                          */
+/* Compiler: =2.4                                                           */
+/*                                                                          */
+/* This file is part of jallib  http://jallib.googlecode.com                */
+/* Released under the BSD license                                           */
+/*                http://www.opensource.org/licenses/bsd-license.php        */
+/*                                                                          */
+/* Description: Rexx script to create a blink-an-LED JAL program in the     */
+/*              in the current default directory for every PIC.             */
+/*              The created program is submitted to the compiler and the    */
+/*              console log is checked for errors and warnings.             */
+/*              When both are 0 the source is copied to the SVN directory   */
+/*              and all compiler but the compiler console log is deleted.   */
+/*                                                                          */
+/* Sources:                                                                 */
+/*                                                                          */
+/* Notes:                                                                   */
+/*  - Written in Classic Rexx style, but requires Object Rexx to run.       */
+/*  - There is no summary of changes maintained for this script.            */
+/*                                                                          */
+/* ------------------------------------------------------------------------ */
+
+parse upper arg runtype selection .             /* where to store jal files */
+
+call RxFuncAdd 'SysLoadFuncs', 'RexxUtil', 'SysLoadFuncs'
+call SysLoadFuncs                               /* load Rexx utilities */
+
+JalV2 = 'k:/c/Jalv2/JalV2.exe'                  /* compiler path (eCS) */
+Validator = 'k:/jallib/tools/jallib validate'   /* validation script */
+
+if runtype = 'TEST' then do                     /* test mode */
+  Include = 'k:/jallib/test/'                   /* test include directory */
+  Options = '-Wall -s' Include                  /* compiler options */
+end
+else do                                         /* normal mode */
+  Include = 'k:/jallib/unvalidated/include/device/'   /* SVN include directory */
+  Options = '-Wno-all -a nul -s' Include        /* no asm output */
+  dst = '\jallib\unvalidated\sample\by_device\' /* destination directory */
+end
+
+if selection = '' then
+  call SysFileTree Include'1*.jal', pic, 'FO'   /* list of device files  */
+else
+  call SysFileTree Include||selection'.jal', pic, 'FO'  /* list of includes  */
+
+if pic.0 < 1 then do
+  say 'No appropriate device files found in directory' Include
+  return 1
+end
+
+k = 0
+
+do i=1 to pic.0
+
+  parse value filespec('Name', pic.i) with PicName '.jal'
+  say PicName
+
+  PgmName = 'b'PicName                          /* program name */
+  PgmFile = 'b'PicName'.jal'                    /* program filespec */
+
+  '@python' validator  pic.i '1>'PgmName'.pyout'  '2>'PgmName'.pyerr'
+  if rc \= 0 then do
+    say 'returncode of validation include file' PicName'.jal is:' rc
+    exit rc
+  end
+  '@erase' PgmName'.py*'                      /* when OK, discard python output */
+
+  call stream  PgmFile, 'c', 'open write replace'
+  call lineout PgmFile, '-- ------------------------------------------------------'
+  call lineout PgmFile, '-- Title: Blink-an-LED of the Microchip PIC'PicName
+  call lineout PgmFile, '--'
+  call lineout PgmFile, '-- Author: Rob Hamerling, Copyright (c) 2008..2008, all rights reserved.'
+  call lineout PgmFile, '--'
+  call lineout PgmFile, '-- Adapted-by:'
+  call lineout PgmFile, '--'
+  call lineout PgmFile, '-- Compiler: =2.4'
+  call lineout PgmFile, '--'
+  call lineout PgmFile, '-- This file is part of jallib',
+                         ' (http://jallib.googlecode.com)'
+  call lineout PgmFile, '-- Released under the BSD license',
+                         '(http://www.opensource.org/licenses/bsd-license.php)'
+  call lineout PgmFile, '--'
+  call lineout PgmFile, '-- Description: Sample blink-an-LED program for Microchip PIC'PicName
+  call lineout PgmFile, '--'
+  call lineout PgmFile, '-- Sources:'
+  call lineout PgmFile, '--'
+  call lineout PgmFile, '-- Notes:'
+  call lineout PgmFile, '--  - File creation date/time:' date('N') time('N')'.'
+  call lineout PgmFile, '--'
+  call lineout PgmFile, '-- ------------------------------------------------------'
+  call lineout PgmFile, '--'
+  call lineout PgmFile, 'include' PicName '                   -- target PICmicro'
+  call lineout PgmFile, '--'
+  call SysFileSearch 'pragma fuse_def OSC', pic.i, osc.
+  if osc.0 > 0 then do                                  /* oscillator pragma present */
+    call SysFileSearch 'HS =', pic.i, osc.
+    if  osc.0 > 0 then do                               /* HS mode supported */
+      call lineout PgmFile, '-- This program assumes a 20 MHz resonator or crystal'
+      call lineout PgmFile, '-- is connected to pins OSC1 and OSC2.'
+      call lineout PgmFile, 'pragma target OSC HS               -- HS crystal or resonator'
+      call lineout PgmFile, 'pragma target clock 20_000_000     -- oscillator frequency'
+    end
+    else do                                             /* assume internal oscillator */
+      call lineout PgmFile, '-- This program assumes the internal oscillator'
+      call lineout PgmFile, '-- is used with a frequency of 4 MHz.'
+      call lineout PgmFile, 'pragma target OSC INTOSC_NOCLKOUT  -- internal oscillator'
+      call lineout PgmFile, 'pragma target clock 4_000_000      -- oscillator frequency'
+      call SysFileSearch ' IOSCFS ', pic.i, ioscfs.
+      if ioscfs.0 > 0 then
+        call lineout PgmFile, 'pragma target IOSCFS  F4MHZ        -- select 4 MHz'
+    end
+  end
+  else do
+    call lineout PgmFile, '-- This program assumes the internal oscillator'
+    call lineout PgmFile, '-- is used with a frequency of 4 MHz.'
+    call lineout PgmFile, 'pragma target clock 4_000_000      -- oscillator frequency'
+    call SysFileSearch ' IOSCFS ', pic.i, ioscfs.
+    if ioscfs.0 > 0 then
+      call lineout PgmFile, 'pragma target IOSCFS  F4MHZ        -- select 4 MHz'
+  end
+  if left(PicName,2) = '18' then
+    call lineout PgmFile, '-- Due to PLL effects the blink frequency may',
+                          'be slower or faster than 2 Hz!'
+  call SysFileSearch 'pragma fuse_def WDT', pic.i, wdt.
+  if wdt.0 > 0 then do
+    call lineout PgmFile, 'pragma target WDT  disabled'
+  end
+  call SysFileSearch 'pragma fuse_def LVP', pic.i, lvp.
+  if lvp.0 > 0 then do
+    call lineout PgmFile, 'pragma target LVP  disabled'
+  end
+  call SysFileSearch 'pragma fuse_def MCLR', pic.i, mclr., 'N'
+  if mclr.0 > 0 then do
+    ln = linein(pic.i, word(mclr.1,1) + 1)              /* line after fuse_def */
+    if pos('EXTERNAL',ln) > 0 then do                   /* MCLR external */
+      call lineout PgmFile, 'pragma target MCLR external'
+    end
+    else do
+      ln = linein(pic.i)                                /* try next line */
+      if pos('EXTERNAL',ln) > 0 then do                 /* MCLR external */
+        call lineout PgmFile, 'pragma target MCLR external'
+      end
+    end
+  end
+  call lineout PgmFile, '--'
+  call lineout PgmFile, 'enable_digital_io()                -- disable analog I/O (if any)'
+  call lineout PgmFile, '--'
+  call lineout PgmFile, '-- You may want to change the selected pin:'
+
+  call stream pic.i, 'c', 'close'
+  port.0 = 3                                            /* ports to scan */
+  port.1 = 'A'
+  port.2 = 'B'
+  port.3 = 'C'
+  do p=1 to port.0
+    do q=0 to 7
+      pinPQ = 'pin_'port.p||q                           /* pin name */
+      call SysFileSearch ' 'pinPQ' ', pic.i, pin.       /* search pin in device file */
+      if pin.0 > 0 then do                              /* pin found */
+        call SysFileSearch ' 'pinPQ'_direction', pic.i, tris.    /* search TRISx */
+        if tris.0 > 0 then do                                /* found */
+          call lineout PgmFile, 'var bit led           is' pinPQ '   -- alias'
+          call lineout PgmFile, pinPQ'_direction = output'
+          leave p
+        end
+        else do                                         /* no TRISx found */
+          say 'Found' pinPQ', but missing' pinPQ'_direction declaration'
+        end
+      end
+    end
+  end
+
+  if p > port.0 then do
+    say 'Error: Could not find suitable I/O pin for LED on' PicName
+    iterate
+  end
+
+  call stream pic.i, 'c', 'close'                       /* done with device file */
+
+  call lineout PgmFile, '--'
+  call lineout PgmFile, 'forever loop'
+  call lineout PgmFile, '   led = on'
+  call lineout PgmFile, '   _usec_delay(250000)'
+  call lineout PgmFile, '   led = off'
+  call lineout PgmFile, '   _usec_delay(250000)'
+  call lineout PgmFile, 'end loop'
+  call lineout PgmFile, '--'
+  call stream PgmFile, 'c', 'close'
+
+
+  '@python' validator PgmFile '1>'PgmName'.pyout' '2>'PgmName'.pyerr'
+  if rc \= 0 then do
+    say 'returncode of validation blink program' PgmFile 'is:' rc
+    exit rc                                     /* terminate! */
+  end
+  '@erase' PgmName'.py*'                        /* when OK, discard log */
+
+  '@'JalV2 Options PgmFile '>'PgmName'.log'     /* compile */
+
+  if rc \= 0 then do                            /* compile error */
+    say 'JalV2 compile error' rc
+    leave                                       /* terminate */
+  end
+
+  '@erase' PgmName'.cod' PgmName'.err' PgmName'.lst' PgmName'.obj' '1>nul 2>nul'
+
+  call SysFileSearch 'WARNING', PgmName'.log', LG.    /* find warning line in log */
+  if LG.0 > 0 then do
+    parse upper var LG.1 errs 'ERRORS,' wngs 'WARNINGS'
+    if errs = 0 & wngs = 0 then do
+      if stream(PgmName'.hex', 'c', 'query exists') = '' then do
+        Say 'Zero warnings and errors, but no hex file. Compiler failure!'
+      end
+      else do
+        k = k + 1                               /* all OK */
+        if runtype \= 'TEST' then do
+          '@copy' PgmFile dst||PicName'\*' '1>nul'
+          '@erase' PgmName'.hex' PgmName'.asm' PgmName'.jal' PgmName'.log' '1>nul 2>nul'
+        end
+      end
+    end
+    else
+      say 'Compilation of' PgmFile 'failed:' LG.1
+  end
+  else do
+    say 'Compilation of' PgmFile 'failed, file' PgmName'.log' 'not found'
+  end
+
+  if k > 500 then                       /* set (low) limit for test purposes */
+    exit k
+
+end
+
+say 'Compiled' k 'of' pic.0 'blink-an-led programs without warnings or errors!'
+
+return 0
+
+
---
+++ branches/jallib-0.1beta/tools/jsg_validator.py
@@ -0,0 +1,253 @@
+#!/usr/bin/python
+#
+#
+# Title: Jallib Style Guide validator script
+# Author: Sebastien Lelong, Copyright (c) 2008, all rights reserved.
+# Adapted-by:
+# Compiler:
+# 
+# This file is part of jallib (http://jallib.googlecode.com)
+# Released under the BSD license (http://www.opensource.org/licenses/bsd-license.php)
+#
+# Sources:
+# 
+# Description: this script analyses a jallib file (JAL file only) and shows warnings and errors,
+#              according to the JSG, Jallib Style Guide. It's supposed to help
+#              having a standard, coherent code withint the jallib repository.
+#              See: http://code.google.com/p/jallib/wiki/JallibStyleGuide for more
+#              
+#
+# Notes:
+#
+
+import sys,os
+import re
+
+def content_not_empty(val):
+	return val.strip() != ''
+def compiler_version(val):
+	return re.match("^(>|<|>=|<=|=)\d+(\.\d+\w*)+\s+$",val)
+
+JALLIB = """^-- This file is part of jallib\s+\(http://jallib.googlecode.com\)"""
+LICENSE = """^-- Released under the BSD license\s+\(http://www.opensource.org/licenses/bsd-license.php\)"""
+
+# exceptions while checking case
+ALLOWED_MIXED_CASE = """.*(pin|port|tris|option|ancon).*"""
+
+FIELDS = [
+		{'field':"Title",'predicate' : content_not_empty,'mandatory': True, 'multiline' :False},
+		{'field':"Author",'predicate':content_not_empty,'mandatory':True,'multiline':False},
+		{'field':"Adapted-by",'predicate':content_not_empty,'mandatory':False,'multiline':False},
+		{'field':"Compiler",'predicate':compiler_version,'mandatory':True,'multiline':False},
+		{'field':"Description",'predicate':content_not_empty,'mandatory':True,'multiline':True},
+		{'field':"Sources",'predicate':content_not_empty,'mandatory':False,'multiline':True},
+		{'field':"Notes",'predicate':content_not_empty,'mandatory':False,'multiline':True},
+	]
+
+errors = []
+warnings = []
+
+def extract_header(content):
+	# reverse() to pop header lines (no such shift())
+	content.reverse()
+	header = []
+	while True:
+		i,l = content.pop()
+		if not l.startswith("--"):
+			# not a header line, put it again
+			content.append((i,l))
+			break
+		else:
+			header.append((i,l))
+	# remaining content: back to forward...
+	content.reverse()
+	return header
+
+def validate_field(data,field,predicate,mandatory,multiline=False):
+
+	syntax = "^-- %s:\s*(.*)" % field
+
+	def single_line_content(line):
+		return "".join(re.sub(syntax,lambda x:x.group(1),line))
+	def multi_line_content(num,line,data):
+		c = single_line_content(line)
+		for i,l in data[data.index((num,line)) + 1:]:
+			# Check no comment gap
+			if not re.match("^--\s",l):
+				errors.append("%d: cannot extract content, line is not starting with comment: %s" % (i,repr(l)))
+				return ""
+			# No other field within content
+			for f in [d['field'] for d in FIELDS]:
+				if l.startswith("-- %s:" % f):
+					errors.append("%d: detected field %s within content of field %s" % (i,f,field))
+					return ""
+			if l.strip() == '--':
+				# got end of block
+				break
+			c += re.sub("^--\s","",l)
+		else:
+			errors.append("%d: cannot find end of field content %s" % (i,field))
+		return c
+		
+	# eat header, and check fields (mandatory or not) and 
+	# content (using predicate)
+	c = None
+	for i,l in data:
+		if re.match(syntax,l):
+			# got it, check field content
+			if multiline:
+				c = multi_line_content(i,l,data)
+			else:
+				c = single_line_content(l)
+			# and check with predicate
+			valid = predicate(c)
+			if mandatory and not valid:
+				errors.append("%d: content for field %s is not valid: %s" % (i,field,repr(c)))
+			break
+	else:
+		if mandatory:
+			errors.append("Cannot find field %s (searched for '%s')" % (field,syntax))
+	
+	return c
+
+
+def validate_header(content):
+	header = extract_header(content)
+		
+	# check stuff about jallib and license
+	jallib = False
+	license = False
+	for i,line in header:
+		if re.match(JALLIB,line):
+			jallib = True
+			if license: break
+		if re.match(LICENSE,line):
+			license = True
+			if jallib: break
+	not jallib and errors.append("Cannot find references to jallib (should have: %s)" % repr(JALLIB))
+	not license and errors.append("Cannot find references to license (should have: %s)" % repr(LICENSE))
+	for field_dict in FIELDS:
+		validate_field(header,**field_dict)
+
+
+def validate_filename(filename):
+	# must be lowercase
+	filename = os.path.basename(filename)
+	if filename.lower() != filename:
+		errors.append("Filename is not lowercase: %s" % filename)
+	# should have "jal" extention
+	if not filename.endswith(".jal"):
+		warnings.append("Filename doesn't have '*.jal' extention: %s" % filename)
+	pass
+
+
+def validate_lower_case(content):
+	# tokenize content, searching for infamous CamelCase words
+	# Also search Capitalized ones...
+	tokenizer = re.compile("\w+")
+	caps = re.compile("[A-Z]")
+	freetext = re.compile('(("|\').*("|\'))')
+	mixed = re.compile(ALLOWED_MIXED_CASE,re.IGNORECASE)
+	# no check in comments
+	codeonly = []
+	for i,line in content:
+		if line.startswith(";") or line.startswith("--"):	
+			continue
+		# don't consider free text (between quotes)
+		line = freetext.sub("",line)
+		l = line.split()
+		def nocomment(l,c):
+			# dirty search comments...
+			if c in l:
+				l = l[:l.index(c)]
+			tmp = [w.startswith(c) for n,w in enumerate(l)]
+			if True in tmp:
+				l = l[:tmp.index(True)]
+			return l
+		# first check ";" *then* "--"
+		# so things like ";;;;-- Note" are not considered
+		# (else produces wrong splitting...)
+		l = nocomment(l,";")
+		l = nocomment(l,"--")
+		# propagate line number
+		codeonly.append((i," ".join(l)))
+		
+	weird = {}	# stores all errors
+	for i,line in codeonly:
+		for token in tokenizer.findall(line):
+			# it can be all in caps (constants)
+			if token.upper() == token:
+				continue
+			# hex definition, give up
+			if "0x" in token:
+				continue
+			# exceptions...
+			if mixed.match(token.lower()):
+				continue
+			if caps.findall(token):
+				weird.setdefault("Found Capitals in token: %s" % repr(token),[]).append(i)
+	# reconsitute errors, with multiple line number if error has occured multiple time
+	for s,nums in weird.items():
+		err = "%s: %s" % (",".join(map(str,nums)),s)
+		errors.append(err)
+
+def validate_procfunc_defs(content):
+	# no () in definition
+	func_proc = re.compile("^(procedure|function)")
+	no_spaces = re.compile(".*\s+\(.*is")
+	for i,line in content:
+		# don't even check both (), not needed
+		if func_proc.match(line) and not "(" in line:
+			errors.append("%d: %s missing (). Calls must also be explicit" % (i,repr(line)))
+		if func_proc.match(line) and no_spaces.match(line):
+			errors.append("%d: found a space before parenthesis: %s" % (i,repr(line)))
+
+def validate_code(content):
+	validate_lower_case(content)
+	validate_procfunc_defs(content)
+	# ...
+
+def validate(filename):
+	validate_filename(filename)
+	# also extract line number (enumerate from 0, count from 1)
+	content = [(i + 1,l) for i,l in enumerate(open(filename,"r").readlines())]
+	validate_header(content)
+	# remaining content has no more header
+	validate_code(content)
+
+
+def report(filename):
+	print "File: %s" % filename
+	print "%d errors found" % len(errors)
+	for err in errors:
+		print "\tERROR: %s" % err
+	print
+	print "%d warnings found" % len(warnings)
+	for warn in warnings:
+		print "\twarning: %s" % warn
+	
+	if errors or warnings:
+		return True
+
+
+if __name__ == '__main__':
+	if len(sys.argv) < 2:
+		print "Please provide a JAL file to validate"
+		sys.exit(255)
+
+	at_least_one_failed = False
+
+	for filename in sys.argv[1:]:
+		validate(filename)
+		if report(filename):
+			at_least_one_failed = True
+		errors = []
+		warnings = []
+
+	if at_least_one_failed:
+		sys.exit(1)
+	else:
+		sys.exit(0)
+
+	
+
