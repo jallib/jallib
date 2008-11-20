7152bcc397070173c5cfab21a82da334f8c70d58
robhamerling <robhamerling@52ce04ab-a751-0410-9c8b-31cceb902f08>
2008-11-20 19:11:20 +0000

    Most recent dev2jal Rexx script version 0.0.53

--- tools/jallib.bat
+++ tools/jallib.bat
@@ -3,19 +3,19 @@ rem  Title: jallib executable for windows system
 rem  Author: Sebastien Lelong, Copyright (c) 2008, all rights reserved.
 rem  Adapted-by:
 rem  Compiler:
-rem  
+rem
 rem  This file is part of jallib (http://jallib.googlecode.com)
 rem  Released under the BSD license (http://www.opensource.org/licenses/bsd-license.php)
-rem 
+rem
 rem  Sources:
-rem  
+rem
 rem  Description: the aim of this script is to ease user's life !
 rem  If *you* are a user, edit this script by specfiying the following
 rem  environment variables. These variables will save you some typing
 rem  while using the jallib wrapper script. Adapt it to your own need !
 rem  Once done, you can put this script in your path.
 rem  Rem: usually, setting JALLIB_ROOT as an absolute path is enough.
- 
+
 
 rem ########################
 rem ENVIRONMENT VARIABLES #
@@ -39,14 +39,14 @@ set JALLIB_JALV2=C:\jalv24h\jalv2.exe
 rem Either:
 rem  - run the "jallibwin.exe" standalone executable
 rem    (no dependencies, recommended)
-rem  - run the jallib.py python script 
+rem  - run the jallib.py python script
 rem    (advanced user, you'll need to install dependencies first)
 
 rem #############
 rem  Standalone #
 rem #############
 rem  (recommended)
-"%JALLIB_ROOT%\tools\jallibwin.exe" %1 %2 %3 %4 %5 %6 %7 %8 %9
+rem "%JALLIB_ROOT%\tools\jallibwin.exe" %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 
 rem ###################
@@ -55,6 +55,6 @@ rem ###################
 rem  (advanced users)
 rem  uncomment the two last line, and comment the standalone call.
 rem # path to "python" executable, if needed
-rem set JALLIB_PYTHON=C:\Python25\python.exe
-rem %JALLIB_PYTHON% %JALLIB_ROOT%/tools/jallib.py %1 %2 %3 %4 %5 %6 %7 %8 %9
+set JALLIB_PYTHON=K:\Python\python.exe
+%JALLIB_PYTHON% %JALLIB_ROOT%/tools/jallib.py %1 %2 %3 %4 %5 %6 %7 %8 %9
 
