155244676ebef98b12611338c7e8478245bf1cf8
sebastien.lelong <sebastien.lelong@52ce04ab-a751-0410-9c8b-31cceb902f08>
2009-04-06 14:03:11 +0000

    pwm: shadow registers initialized to 0 to make libs compatible with Enhanced-PWM modules

--- trunk/tools/pwm_generate_all.sh
+++ trunk/tools/pwm_generate_all.sh
@@ -2,7 +2,7 @@
 
 for num in `echo 1 2 3 4 5`
 do
-	python pwm_ccpx_generator.py pwm_ccpx.tmpl $num > ../unvalidated/include/peripheral/pwm/pwm_ccp$num.jal
+	python pwm_ccpx_generator.py pwm_ccpx.tmpl $num > ../include/peripheral/pwm/pwm_ccp$num.jal
 done
 
 
