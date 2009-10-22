4437f3f9e64d8318c52e4dc6f8883dcc1f2cdc99
sebastien.lelong <sebastien.lelong@52ce04ab-a751-0410-9c8b-31cceb902f08>
2009-10-22 06:03:57 +0000

    dita2html wrapper + script to prepare HTML content for Drupal

---
+++ tools/japp/dita2html.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+DITAFILE=`pwd`/$1
+OUTPUTDIR=$2
+TMPDIR=${OUTPUTDIR}.tmp
+
+if [ "$DITA_HOME" = "" ]
+then
+    echo "DITA_HOME must be set to DITA installation directory"
+	exit 255
+fi
+
+if [ "$DITAFILE" = "" ] || [ "$OUTPUTDIR" = "" ]
+then
+    echo "Please provide a DITA file and an outpur directory"
+    exit 255
+fi
+
+# clean outputdir
+if ! test -d $OUTPUTDIR 
+then
+    mkdir -p $OUTPUTDIR
+else
+    rm -fr $OUTPUTDIR/*
+fi
+if ! test -d $TMPDIR
+then
+    mkdir $TMPDIR
+else
+    rm -fr $TMPDIR/*
+fi
+
+# -Duser.csspath.url=  ---> don't copy css files
+ant -Dargs.transtype=xhtml -Duser.csspath.url= -Doutput.dir=$OUTPUTDIR -Dargs.input=$DITAFILE -Dargs.logdir=$TMPDIR -Ddita.temp.dir=$TMPDIR -Dargs.target=init -f $DITA_HOME/build.xml dita2xhtml
