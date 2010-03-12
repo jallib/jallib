94e02b245b11fd5a1ce71782a13d09dda000e2e0
sebastien.lelong <sebastien.lelong@52ce04ab-a751-0410-9c8b-31cceb902f08>
2010-03-12 19:46:50 +0000

    reindent

--- trunk/tools/adc/adc_channels_generator.py
+++ trunk/tools/adc/adc_channels_generator.py
@@ -18,32 +18,33 @@
 import Cheetah.Template
 
 if __name__ == "__main__":
-	import sys
-	# Prepare template
-	tmpl_file = "adc_channels.jal.tmpl"
-	tmplsrc = "".join(file(tmpl_file,"r").readlines())
-	klass = Cheetah.Template.Template.compile(tmplsrc)
-	tmpl = klass()
-	# ADC pins grouped + whole pin map
-	from pinmap import pinmap
-	from adc_pcfg import adc_pcfg
-	from adc_pins import adc_pins
-	from pic_ds_map import pic_ds, ds_pic
+    import sys
+    # Prepare template
+    tmpl_file = "adc_channels.jal.tmpl"
+    tmplsrc = "".join(file(tmpl_file,"r").readlines())
+    klass = Cheetah.Template.Template.compile(tmplsrc)
+    tmpl = klass()
+    # ADC pins grouped + whole pin map
+    from pinmap import pinmap
+    from adc_pcfg import adc_pcfg
+    from adc_pins import adc_pins
+    from pic_ds_map import pic_ds, ds_pic
 
-	# extract PICs where PCFG is used to configure dependent ADC channels
-	dependent_pcfg_dsref = adc_pcfg.keys()
-	pcfg_combination_pics = []
-	[pcfg_combination_pics.extend(ds_pic[r]) for r in dependent_pcfg_dsref]
+    # extract PICs where PCFG is used to configure dependent ADC channels
+    dependent_pcfg_dsref = adc_pcfg.keys()
+    pcfg_combination_pics = []
+    for r in dependent_pcfg_dsref:
+        pcfg_combination_pics.extend(ds_pic[r])
 
-	tmpl.pinmap = pinmap
-	tmpl.pcfg_combination_pics = pcfg_combination_pics 
-	tmpl.adc_pcfg = adc_pcfg
-	tmpl.pic_ds = pic_ds
-	tmpl.ds_pic = ds_pic
-	tmpl.adc_pins = adc_pins
-	
-	fout = file("adc_channels.jal","w")
-	print >> fout,tmpl.main()
-	fout.close()
-	sys.exit(0)
+    tmpl.pinmap = pinmap
+    tmpl.pcfg_combination_pics = pcfg_combination_pics 
+    tmpl.adc_pcfg = adc_pcfg
+    tmpl.pic_ds = pic_ds
+    tmpl.ds_pic = ds_pic
+    tmpl.adc_pins = adc_pins
+    
+    fout = file("adc_channels.jal","w")
+    print >> fout,tmpl.main()
+    fout.close()
+    sys.exit(0)
 
