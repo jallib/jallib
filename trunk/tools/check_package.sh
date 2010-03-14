f23c1e4e6f1c77fb703c5ef3d58527fd855370cc
sebastien.lelong <sebastien.lelong@52ce04ab-a751-0410-9c8b-31cceb902f08>
2010-03-14 09:25:44 +0000

    improve bee report failure while checking, clean makefile, added usb_keyboard.jal to TORELEASE to fix bee package

--- trunk/tools/check_package.sh
+++ trunk/tools/check_package.sh
@@ -5,16 +5,28 @@ LIBDIR=$1
 SAMPLEDIR=$2
 COMPILERBIN=$3
 
+TMPFILE=$HOME/tmp/compile.log
+FAILED=$HOME/tmp/failed.log
+
+rm -f $FAILED
 
 status=0
 for f in `find $SAMPLEDIR -name \*.jal`
 do
-	$COMPILERBIN -s $LIBDIR $f > /dev/null 2>&1 
+	$COMPILERBIN -s $LIBDIR $f > $TMPFILE 2>&1 
 	if [ "$?" != "0" ]
 	then
 		echo "$f failed !"
+        echo "$f failed !" >> $FAILED
+        cat $TMPFILE >> $FAILED
+        echo >> $FAILED
 		status=1
 	fi
 done
+
+echo
+echo
+cat $FAILED
+rm -f $FAILED
 
 exit $status
