86d4f76b29163f829e8cdbcfe90e2976baea7c61
jsuijs <jsuijs@52ce04ab-a751-0410-9c8b-31cceb902f08>
2010-05-02 09:15:28 +0000

    Refactoring: relevant context now passed as parameter.

--- trunk/grammar/symboltable.h
+++ trunk/grammar/symboltable.h
@@ -1,11 +1,23 @@
 // symboltable.h
-
 #include <stdio.h>
 
-// size is fixed but large.
-#define MAX_NR_PARAMS      20
-
-// dit is een parameter van een procedure
+// Overall structure:
+//    
+//    - The context struct contains a list of elements (content).
+//    - The context struct optioinal points to a wider context.
+//    - The global context is stored separate and should be checks after the widere contexts.
+//    (about to implement the above ;)
+// 
+//    The elements in the list are 'symbol' structs, with name, type and pointer to details.
+// 
+//    There are two main types of symbols: 
+//       - VAR - contains info on a var, can be normal var, pseudo var or both
+//       - FUNCTION (or procedure which is function with void return). Function has 
+//         a param-struct for each param.
+     
+//-----------------------------------------------------------------------------
+// Function/Procedure parameter structure
+//-----------------------------------------------------------------------------
 typedef struct SymbolParam_struct{ 
    struct SymbolParam_struct *next;
    
@@ -14,8 +26,9 @@ typedef struct SymbolParam_struct{
    char     CallMethod; // v = value, r = reference, c = code
 } SymbolParam;
 
-
-// function / procedure related	                                                            
+//-----------------------------------------------------------------------------
+// Function/Procedure struct
+//-----------------------------------------------------------------------------
 typedef struct SymbolFunction_struct{
 
    int      ReturnType;   
@@ -25,21 +38,22 @@ typedef struct SymbolFunction_struct{
    
 } SymbolFunction;
 
-
-// pvar related (pseudo-var, volatile)
-
-typedef struct Pvar_stuct {          
+//-----------------------------------------------------------------------------
+// Variable struct 
+//-----------------------------------------------------------------------------
+typedef struct Var_stuct {          
    char *put;     // put function name
    char *get;     // get function name
    char *data;    // data field name 
    int   size;    // PV data size in bytes (round up)
    char  p1;      // Param1, implementation-dependent
    char  p2;      // Param2, implementation-dependent
-} Pvar;
+} Var;
 
-// main symbol stuct 
+//-----------------------------------------------------------------------------
+// Symbol stuct 
+//-----------------------------------------------------------------------------
 typedef struct Symbol_type{
-	struct Symbol_type    *Prev;
 	struct Symbol_type    *Next;
 	
 	char     *Name;
@@ -49,32 +63,50 @@ typedef struct Symbol_type{
    
 } Symbol;
 
+//-----------------------------------------------------------------------------
+// Context struct
+//-----------------------------------------------------------------------------
+typedef struct Context_type{
+
+	Symbol  *Head;
+   Symbol  *Last;
+
+   int      IsGlobal;   
+	struct Context_type   *Wider; // wider context
+	
+} Context;
+
+
+// Types of symbol details
+#define S_FUNCTION 1
+#define S_VAR      2
+
+
+// external vars & prototypes
+
 extern pANTLR3_UINT8   jalParserTokenNames[];
-extern Symbol *SymbolTail;  // points to most recent symbol
-extern Symbol *SymbolHead;  // points to oldest symbol
+extern Context *GlobalContext;
 
 char *CreateName(char *Name);
                            
-void DumpSymbolTable(void);
-Symbol *GetSymbolPointer  (char *SymbolName);
+void DumpSymbolTable(Context *co);
+Symbol *GetSymbolPointer  (Context *co, char *SymbolName);
 
 SymbolParam *SymbolFunctionAddParam(SymbolFunction *f, int TokenType);
 void SymbolParamSetName(SymbolParam *p, char *Name);
 void DumpSymbol(Symbol *s);
-Symbol *NewSymbolFunction(void);
+Symbol *NewSymbolFunction(Context *co);
+
+void SymbolPrintVarTable(Context *co);
+Var *SymbolGetVar(Context *co, char *SymbolName);
+Var *SymbolGetOrNewVar(Context *co, char *Name);
+void SymbolVarAdd_PutName(Context *co, char *BaseName, char *PutName);
+void SymbolVarAdd_GetName(Context *co, char *BaseName, char *GetName);
+void SymbolVarAdd_DataName(Context *co, char *BaseName, char *DataName);
 
 
+void CreateGlobalContext(void);
 
-void SymbolPrintPvarTable(void);
-Pvar *SymbolGetPvar(char *Name);
-Pvar *SymbolGetOrAddPvar(char *Name);
-void SymbolPvarAdd_PutName(char *BaseName, char *PutName);
-void SymbolPvarAdd_GetName(char *BaseName, char *GetName);
-void SymbolPvarAdd_DataName(char *BaseName, char *DataName);
+Context *NewContext(Context *WiderContext);
 
 
-
-
-
-#define S_FUNCTION 1
-#define S_PVAR     2
