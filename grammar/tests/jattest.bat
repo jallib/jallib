07832a9c0b224bf4d73e7b4592e2e5ae551794b0
jsuijs <jsuijs@52ce04ab-a751-0410-9c8b-31cceb902f08>
2010-05-06 05:35:04 +0000

    JAT testframe working, 3 tests pass.

--- grammar/tests/jattest.bat
+++ grammar/tests/jattest.bat
@@ -1,16 +1,17 @@
-rem @echo off
+@echo off
 if NOT "%1"=="" goto parameter
-
-rem ---------------------------------------------------------------------------
-rem start of list here
-rem ---------------------------------------------------------------------------
 
 del alloutput.txt
 del all.diff
 
+rem ---------------------------------------------------------------------------
+rem start of list here
+rem ---------------------------------------------------------------------------
+                  
+                  
 call %0 test_001
-rem call %0 test02
-rem call %0 test03
+call %0 test_002
+call %0 test_003
 
 
 rem ---------------------------------------------------------------------------
@@ -28,17 +29,32 @@ Rem run JAT on a (one) test and compare the result with the reference
 
 del test.c
 del test.exe
-del test.txt
+del test.txt 
+del test.tmp
 
 echo include ../pc_target        >test.jal
 type %1.jal                     >>test.jal
 ..\jalparser test.jal            >test.c
 gcc test.c -otest.exe
-test.exe                         >test.txt
-diff test.txt %1.txt             >test.diff
+test.exe                         >test.tmp
+perl comcapture.pl -f test.tmp   >test.txt
 
-test test.diff                  >>all.diff
-type testoutput.txt             >> alloutput.txt
+copy test.txt                    %1.txt   >nul
+diff %1.txt %1.ref              >test.diff
+
+rem --------------------- message of SUCCES or FAILED
+if errorlevel 1 goto diff_err
+echo TEST %1 SUCCES
+goto diff_done
+          
+: diff_err
+echo TEST %1 FAILED          
+
+:diff_done
+rem ----------------- END message of SUCCES or FAILED
+
+type test.diff                  >>all.diff
+type test.txt                   >> alloutput.txt
 
 rem ---------------------------------------------------------------------------
 rem end of test-one here
