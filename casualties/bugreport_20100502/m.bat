9813562265912be8aa36e501bfdd5ed46e8f2174
jsuijs <jsuijs@52ce04ab-a751-0410-9c8b-31cceb902f08>
2010-06-13 18:15:44 +0000

    bugreport cleanup

--- casualties/bugreport_20100502/m.bat
+++
@@ -1,7 +0,0 @@
-copy  16f877a_serial_print_ok.jal jallib.jal
-c:\jallib\compiler\jalv2 -debug jallib.jal
-copy jallib.asm jallib_ok.asm
-
-copy  16f877a_serial_print_err.jal jallib.jal
-c:\jallib\compiler\jalv2 -debug jallib.jal
-copy jallib.asm jallib_err.asm
