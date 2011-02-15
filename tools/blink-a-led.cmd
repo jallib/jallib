5d61cb06e39d1b2d33608d22a50202d107db180b
robhamerling <robhamerling@52ce04ab-a751-0410-9c8b-31cceb902f08>
2011-02-15 09:07:03 +0000

    Most recent dev2jal Rexx script version 0.1.16

--- tools/blink-a-led.cmd
+++ tools/blink-a-led.cmd
@@ -23,7 +23,7 @@
  *                                                                          *
  * Sources:                                                                 *
  *                                                                          *
- * Version: 0.0.3                                                           *
+ * Version: 0.0.4                                                           *
  *                                                                          *
  * Notes:                                                                   *
  *  - Uses Classic Rexx.                                                    *
@@ -36,38 +36,44 @@
 
 parse upper arg runtype selection .             /* where to store jal files */
 
-call RxFuncAdd 'SysLoadFuncs', 'RexxUtil', 'SysLoadFuncs'
-call SysLoadFuncs                               /* load Rexx utilities */
-
-JalV2 = '\c\jalv2\bin\JalV2ecs.exe'             /* latest compiler */
-Validator = '/jallib/tools/jallib.py validate'  /* validation command */
-
-if runtype = 'TEST' then do                     /* test mode */
-  Include = '/jal/dev2jal/test'                 /* Test device files */
-  dst = './'                                    /* current directory */
-end
-else if runtype = 'PROD' then do                /* normal mode */
-  Include = '/jallib/include/device'            /* Jallib device files */
-  dst = '/jallib/sample/'                       /* sample dir */
-end
-else do                                         /* normal mode */
+if runtype \= 'PROD' & runtype \= 'TEST' then do
   say 'Error: Required argument missing: "prod" or "test"'
   return 1
 end
 
-Options = '-no-codfile -no-asm -s' Include
+call RxFuncAdd 'SysLoadFuncs', 'RexxUtil', 'SysLoadFuncs'
+call SysLoadFuncs                               /* load Rexx utilities */
+
+
+Validator = '/jallib/tools/jallib.py validate'  /* validation command */
+
+if runtype = 'PROD' then do                     /* test mode */
+  JalV2 = '\jallib\compiler\JalV2ecs.exe'       /* prod compiler */
+  include = '/jallib/include/device;/jallib/include/jal'   /* JalV2 include specification */
+  dst = '/jallib/sample/'                       /* blink-samples destination */
+  src = '/jallib/include/device'                /* source for samples */
+end
+else do                                         /* test mode */
+  JalV2 = '\c\jalv2\bin\JalV2ecs.exe'           /* latest compiler */
+  Include = '/jal/dev2jal/test;/jallib/include/device;/jallib/include/jal'   /* test + prod device files */
+  dst = './'                                    /* to current directory */
+  src = '/jal/dev2jal/test'                     /* test device files */
+  if stream(src'/chipdef_jallib.jal', 'c', 'query exists') = '' then   /* no test device files */
+    src = '/jallib/include/device'              /* production device files */
+end
 
 if selection = '' then
-  call SysFileTree Include'/1*.jal', pic, 'FO'   /* list of device files  */
+  call SysFileTree src'/1*.jal', pic, 'FO'         /* all device files  */
 else
-  call SysFileTree Include'/'selection'.jal', pic, 'FO'  /* list of device files  */
+  call SysFileTree src'/'selection'.jal', pic, 'FO'  /* selected device files  */
 
 if pic.0 < 1 then do
-  say 'No appropriate device files found in directory' Include
+  say 'No appropriate device files found in directory' src
   return 1
 end
 
 call envir 'python'                             /* set Python environment */
+Options = '-no-codfile -no-asm -s' Include      /* compiler options */
 
 k = 0
 
@@ -76,7 +82,7 @@ do i=1 to pic.0
   parse value filespec('Name', pic.i) with PicName '.jal'
   say PicName
 
-  '@python' validator Include'/'PicName'.jal' '1>'PicName'.pyout' '2>'PicName'.pyerr'
+  '@python' validator src'/'PicName'.jal' '1>'PicName'.pyout' '2>'PicName'.pyerr'
   if rc \= 0 then do
     say 'Validation of device file for' PicName 'failed, rc' rc
     leave                                       /* terminate! */
@@ -92,7 +98,7 @@ do i=1 to pic.0
   call lineout PgmFile, '-- ------------------------------------------------------'
   call lineout PgmFile, '-- Title: Blink-a-led of the Microchip pic'PicName
   call lineout PgmFile, '--'
-  call lineout PgmFile, '-- Author:' ScriptAuthor', Copyright (c) 2008..2010, all rights reserved.'
+  call lineout PgmFile, '-- Author:' ScriptAuthor', Copyright (c) 2008..2011, all rights reserved.'
   call lineout PgmFile, '--'
   call lineout PgmFile, '-- Adapted-by:'
   call lineout PgmFile, '--'
@@ -126,14 +132,14 @@ do i=1 to pic.0
           leave j
         end
       end
-      say 'Selected pragma OSC' hs
+      if tuntype = 'TEST' then
+         say 'Selected pragma OSC' hs
       call lineout PgmFile, '-- This program assumes that a 20 MHz resonator or crystal'
       call lineout PgmFile, '-- is connected to pins OSC1 and OSC2.'
-      if left(PicName,2) = '18' then
-        call lineout PgmFile, '-- (unspecified configuration bits may cause a different frequency!)('
+      call lineout PgmFile, '-- (unspecified configuration bits may cause a different frequency!)'
       call lineout PgmFile, 'pragma target clock 20_000_000     -- oscillator frequency'
       call lineout PgmFile, '-- configuration memory settings (fuses)'
-      call lineout PgmFile, 'pragma target OSC  'left(hs,7)'         -- HS crystal or resonator'
+      call lineout PgmFile, 'pragma target OSC      'left(hs,7)'          -- HS crystal or resonator'
     end
     else do                                             /* assume internal oscillator */
       call lineout PgmFile, '-- This program assumes the internal oscillator'
@@ -144,12 +150,12 @@ do i=1 to pic.0
       if osc.0 > 0 then do                              /* found internal oscillator */
         do j = 1 to osc.0
           if pos('NOCLK', osc.j) > 0 then do            /* search for noclkout */
-             call lineout PgmFile, 'pragma target OSC  'word(osc.j,1)'     -- internal oscillator'
+             call lineout PgmFile, 'pragma target OSC      'word(osc.j,1)'     -- internal oscillator'
              leave                                      /* done! */
           end
         end
         if j > osc.0 then                               /* not 'noclkout': take first */
-          call lineout PgmFile, 'pragma target OSC  'word(osc.1,1)'     -- internal oscillator'
+          call lineout PgmFile, 'pragma target OSC       'word(osc.1,1)'    -- internal oscillator'
       end
       call SysFileSearch ' IOSCFS ', pic.i, ioscfs.
       if ioscfs.0 > 0 then
@@ -159,37 +165,35 @@ do i=1 to pic.0
   else do
     call lineout PgmFile, '-- This program assumes the internal oscillator'
     call lineout PgmFile, '-- is running at a frequency of 4 MHz.'
-    call lineout PgmFile, 'pragma target clock 4_000_000      -- oscillator frequency'
+    call lineout PgmFile, 'pragma target clock 4_000_000     -- oscillator frequency'
     call lineout PgmFile, '-- configuration memory settings (fuses)'
     call SysFileSearch ' IOSCFS ', pic.i, ioscfs.
     if ioscfs.0 > 0 then
-      call lineout PgmFile, 'pragma target IOSCFS  F4MHZ        -- select 4 MHz'
+      call lineout PgmFile, 'pragma target IOSCFS   F4MHZ      -- select 4 MHz'
   end
   call SysFileSearch 'fuse_def PLLEN', pic.i, pll.
   if pll.0 > 0 then do
     if pos('intosc', pll.1) = 0 then                        /* PLL, not for intosc */
-      call lineout PgmFile, 'pragma target PLLEN  P1            -- PLL off'
+      call lineout PgmFile, 'pragma target PLLEN    P1         -- PLL off'
   end
   call SysFileSearch 'fuse_def PLLDIV', pic.i, pll.
-  if pll.0 > 0 then do
-    call lineout PgmFile, 'pragma target PLLDIV  P1           -- PLL off'
-  end
+  if pll.0 > 0 then
+    call lineout PgmFile, 'pragma target PLLDIV   P1           -- PLL off'
   call SysFileSearch 'fuse_def CPUDIV', pic.i, pll.
-  if pll.0 > 0 then do
-    call lineout PgmFile, 'pragma target CPUDIV  P2           -- no cycle divisor'
-  end
+  if pll.0 > 0 then
+    call lineout PgmFile, 'pragma target CPUDIV   P2           -- no cycle divisor'
   call SysFileSearch 'fuse_def WDT', pic.i, wdt.
-  if wdt.0 > 0 then do
-    call lineout PgmFile, 'pragma target WDT  disabled        -- no watchdog'
-  end
+  if wdt.0 > 0 then
+    call lineout PgmFile, 'pragma target WDT      disabled     -- no watchdog'
   call SysFileSearch 'fuse_def XINST', pic.i, xinst.
-  if xinst.0 > 0 then do
-    call lineout PgmFile, 'pragma target XINST disabled       -- not supported by JalV2'
-  end
+  if xinst.0 > 0 then
+    call lineout PgmFile, 'pragma target XINST    disabled     -- not supported by JalV2'
+  call SysFileSearch 'fuse_def DEBUG', pic.i, debug.
+  if debug.0 > 0 then
+    call lineout PgmFile, 'pragma target DEBUG    disabled     -- no debugging'
   call SysFileSearch 'fuse_def LVP', pic.i, lvp.
-  if lvp.0 > 0 then do
-    call lineout PgmFile, 'pragma target LVP  disabled        -- no Low Voltage Programming'
-  end
+  if lvp.0 > 0 then
+    call lineout PgmFile, 'pragma target LVP      disabled     -- no Low Voltage Programming'
   call SysFileSearch 'fuse_def MCLR', pic.i, mclr., 'N'
   if mclr.0 > 0 then do
     do  word(mclr.1,1)                                  /* skip lines ahead of MCLR */
@@ -197,12 +201,12 @@ do i=1 to pic.0
     end
     ln = linein(pic.i)                                  /* line after fuse_def MCLR */
     if pos('EXTERNAL',ln) > 0 then do                   /* MCLR external */
-      call lineout PgmFile, 'pragma target MCLR external        -- reset externally'
+      call lineout PgmFile, 'pragma target MCLR     external     -- reset externally'
     end
     else do
       ln = linein(pic.i)                                /* try next line */
       if pos('EXTERNAL',ln) > 0 then do                 /* MCLR external */
-        call lineout PgmFile, 'pragma target MCLR external        -- reset externally'
+        call lineout PgmFile, 'pragma target MCLR     external     -- reset externally'
       end
     end
   end
@@ -213,8 +217,6 @@ do i=1 to pic.0
   call lineout PgmFile, 'enable_digital_io()                -- make all pins digital I/O'
   call lineout PgmFile, '--'
   call lineout PgmFile, '-- Specify the pin to which the LED (with serial resistor!) is connected:'
-
-  call stream pic.i, 'c', 'close'
   port.0 = 3                                            /* ports to scan */
   port.1 = 'A'
   port.2 = 'B'
@@ -236,13 +238,11 @@ do i=1 to pic.0
       end
     end
   end
-
+  call stream pic.i, 'c', 'close'                       /* done with device file */
   if p > port.0 then do
     say 'Error: Could not find suitable I/O pin for LED on' PicName
     iterate
   end
-
-  call stream pic.i, 'c', 'close'                       /* done with device file */
 
   call lineout PgmFile, '--'
   call lineout PgmFile, 'forever loop'
