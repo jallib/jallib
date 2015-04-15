cfcadab3c4b8ea50c6a73f0710a2b340bb72ba69
robhamerling <robhamerling@gmail.com>
2015-04-15 20:02:18 +0200

    Modified tools file createdswiki.py to generated Markdown (and inline html for tables)

--- tools/createdswikis.py
+++ tools/createdswikis.py
@@ -24,13 +24,13 @@ import json
 
 #  input (may require changes for other systems or platforms)
 home        = os.path.join("/", "home", "robh")             # user home directory
-base        = os.path.join("/", "media", "NAS")             # base of MPLAB-X material  
-picspecfile = os.path.join(home,"jallib","tools","devicespecific.json")   # PIC specific properties
-dslist      = os.path.join(home,"jallib","tools","datasheet.list")        # datasheet number/title file
-pdfdir      = os.path.join(base,"PicDatasheets")                          # dir with datasheets
+base        = os.path.join("/", "media", "nas")             # base of MPLAB-X material  
+picspecfile = os.path.join(base,"jallib","tools","devicespecific.json")   # PIC specific properties
+dslist      = os.path.join(base,"jallib","tools","datasheet.list")        # datasheet number/title file
+pdfdir      = os.path.join(base,"picdatasheets")                          # dir with datasheets
 
 #  output
-dst         = os.path.join(home, "jallib","wiki")                         # output path
+dst         = os.path.join(home, "jallib", "jallib.wiki")                 # output path
 
 url         = "http://ww1.microchip.com/downloads/en/DeviceDoc/"          # Microchip site
 
@@ -40,12 +40,13 @@ def read_datasheet(infile):
    fp = open(infile, "r")                             # datasheet list
    for ln in fp:
       w = ln.split(" ", 1)                            # separate number from title
-      dd = w[0].upper()
-      ds = w[1].strip()
-      key = int(dd[0:len(dd)-1])                      # strip letter; get numeric key
-      fd = os.path.getmtime(os.path.join(pdfdir, dd + ".pdf"))
-      fd = time.strftime("%Y/%m", time.gmtime(fd))    # year/month
-      datasheet[key] = {"NUMBER" : dd, "TITLE" : ds, "DATE" : fd}
+      if (len(w) > 1):
+         dd = w[0].upper()
+         ds = w[1].strip()
+         key = int(dd[0:len(dd)-1])                      # strip letter; get numeric key
+         fd = os.path.getmtime(os.path.join(pdfdir, dd.lower() + ".pdf"))
+         fd = time.strftime("%Y/%m", time.gmtime(fd))    # year/month
+         datasheet[key] = {"NUMBER" : dd, "TITLE" : ds, "DATE" : fd}
 
 
 def pic_wiki(outfile):
@@ -58,8 +59,9 @@ def pic_wiki(outfile):
       title = "PICname - DataSheet cross reference"
    print "Building", title
    fp = open(os.path.join(dst,outfile), "w")
-   fp.write("#summary " + title + "\n\n----\n\n= " + title + " =\n\n")
-   fp.write("|| *PIC type* || *Number* || *Date* || *Datasheet Title* ||\n")
+   fp.write("#" + title + "\n\n")
+   fp.write("<table>\n")
+   fp.write("<tr><th>PIC type</th><th>Number</th><th>Date</th><th>Datasheet Title</th></tr>\n")
    piclist = picspec.keys()
    piclist.sort()
    for pic in piclist:
@@ -72,16 +74,16 @@ def pic_wiki(outfile):
          key = int(ds)                                   # numeric index!
          if key in datasheet:                            # DS present
             if datasheet[key].get("TITLE") != None:      # title present
-               fp.write("|| %-12s || " % pic
-                        + '<a href="' + url + datasheet[key].get("NUMBER") + '.pdf">'
-                        + datasheet[key].get("NUMBER") + "</a> || "
-                        + datasheet[key].get("DATE")   + " || "
-                        + datasheet[key].get("TITLE")  + " ||\n")
+               fp.write("<tr><td>%-12s</td>" % pic
+                        + "<td>" + '<a href="' + url + datasheet[key].get("NUMBER") + '.pdf">'
+                        + datasheet[key].get("NUMBER") + "</a></td>"
+                        + "<td>" + datasheet[key].get("DATE")   + "</td>"
+                        + "<td>" + datasheet[key].get("TITLE")  + "</td></tr>\n")
             else:
                print "No title found of datasheet", key
          else:
             print "Datasheet number", key, "of", pic, "in", picspecfile, "not found"
-   fp.write("\n")
+   fp.write("</table>\n")
    fp.close()
 
 
@@ -114,34 +116,36 @@ def group_wiki(outfile):
          else:
             groups[key].append(pic)
             groups[key].sort()                              # keep list in sequence
-
    fp = open(os.path.join(dst,outfile), "w")
-   fp.write("#summary " + title + "\n\n----\n\n= " + title + " =\n\n")
-   fp.write("== Sorted on DataSheet number ==\n")
-   fp.write("=== (see below for a list sorted on PIC type) ===\n\n")
-   fp.write("|| *DataSheet* || *Date* || *PIC type* ||\n")
+   fp.write("#" + title + "\n")
+   fp.write("##Sorted on DataSheet number\n")
+   fp.write("###(see below for a list sorted on PIC type)\n\n")
+   fp.write("<table>\n")
+   fp.write("<tr><th>DataSheet</th><th>Date</th><th>PIC type</th></tr>\n")
    grouplist = groups.keys()
    grouplist.sort()
    for group in grouplist:
       if group in datasheet:
-         fp.write("|| " + '<a href="' + url + datasheet[group].get("NUMBER") + '.pdf">'
-                   + datasheet[group].get("NUMBER") + "</a> || "
-                   + datasheet[group].get("DATE")   + " || "
-                   + " ".join(groups[group]) + "||\n")
+         fp.write("<tr><td>" + '<a href="' + url + datasheet[group].get("NUMBER") + '.pdf">'
+                   + datasheet[group].get("NUMBER") + "</a></td>"
+                   + "<td>"+ datasheet[group].get("DATE")   + "</td>"
+                   + "<td>"+ " ".join(groups[group]) + "</td></tr>\n")
       else:
          print "Datasheet", group, "not found"
-   fp.write("\n\n----\n\n= " + title + " =\n\n")
-   fp.write("== Sorted on PIC type (lowest in the group) ==\n\n")
-   fp.write("|| *DataSheet* || *Date* || *PIC type* ||\n")
+   fp.write("</table>\n\n")
+   fp.write("#" + title + "\n")
+   fp.write("##Sorted on PIC type (lowest in the group)\n\n")
+   fp.write("<table>\n")
+   fp.write("<tr><th>DataSheet</th><th>Date</th><th>PIC type</th></tr>\n")
    grouplist = groups.keys()
    grouplist.sort(key = sort_group_on_pic)
    for group in grouplist:
       if group in datasheet:
-         fp.write("|| " + '<a href="' + url + datasheet[group].get("NUMBER") + '.pdf">'
-                   + datasheet[group].get("NUMBER") + "</a> || "
-                   + datasheet[group].get("DATE")   + " || "
-                   + " ".join(groups[group]) + "||\n")
-   fp.write("\n")
+         fp.write("<tr><td>" + '<a href="' + url + datasheet[group].get("NUMBER") + '.pdf">'
+                   + datasheet[group].get("NUMBER") + "</a></td>"
+                   + "<td>"+ datasheet[group].get("DATE")   + "</td>"
+                   + "<td>" + " ".join(groups[group]) + "</td></tr>\n")
+   fp.write("</table>\n")
    fp.close()
 
 
@@ -150,8 +154,8 @@ if __name__ == "__main__":
    datasheet = {}                                           # new dictionary
    read_datasheet(dslist)                                   # load datasheet info
    picspec = json.load(file(picspecfile))                   # load PIC specific info
-   pic_wiki("DataSheets.wiki")
-   pic_wiki("ProgrammingSpecifications.wiki")
-   group_wiki("PicGroups.wiki")
-   group_wiki("PicPgmGroups.wiki")
+   pic_wiki("DataSheets.md")
+   pic_wiki("ProgrammingSpecifications.md")
+   group_wiki("PicGroups.md")
+   group_wiki("PicPgmGroups.md")
 
