d8edae1128efbb12a0434623ad47733916b2174b
Rob Jansen <12682653+RobJansen62@users.noreply.github.com>
2020-03-07 11:42:18 +0100

    Test script and Travis CI file for testing continuous build.

---
+++ build.py
@@ -0,0 +1,38 @@
+"""
+Title: Generate a new build using Travis CI
+
+Author: Rob Jansen, Copyright (c) 2020..2020, all rights reserved.
+
+Adapted-by: 
+
+Revision: $Revision$
+
+Compiler: N/A
+
+This file is part of jallib  https://github.com/jallib/jallib
+Released under the BSD license https://www.opensource.org/licenses/bsd-license.php
+
+Description:
+   Create a new build by validating and compiling all samples mentioned in TORELASE.
+   A build is started after each commit.
+
+Sources: N/A
+
+Notes:
+   
+
+"""
+
+# ----------------------------
+# --- E N T R Y  P O I N T ---
+# ----------------------------
+if (__name__ == "__main__"):
+   """ Start process
+   """
+
+   print("For now this is just a test!")
+
+
+
+
+
