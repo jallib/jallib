1661212c0b1c5fd8a2f602a6b113a0fb272b71f1
Rob Jansen <rob_cj@hotmail.com>
2025-12-28 11:30:48 +0100

    Update convert_bmp_to_jal_array.py
    Added option to create an array using Big Endian instead of Little Endian.

--- project/glcd_ili9341/convert_bmp_to_jal_array.py
+++ project/glcd_ili9341/convert_bmp_to_jal_array.py
@@ -1,6 +1,6 @@
 # ------------------------------------------------------------------------------------
 # Title: JAL bitmap image array generation script  
-# Author: Rob Jansen, Copyright (c) 2022..2022, all rights reserved.
+# Author: Rob Jansen, Copyright (c) 2022..2025, all rights reserved.
 # Python version : 3.8
 #
 # This file is part of jallib (https://github.com/jallib/jallib)
@@ -11,6 +11,7 @@
 # Notes:       A way to generate the RGB565 bitmap from a png or jpg file:
 #              Converter: https://online-converting.com/image/convert2bmp/
 #              Choose: 16 (5:6:5 RGB Hi-Color), rows direction Top - Bottom
+#              If needed set a new DPI size. DPI must be an even number for this script!
 #
 
 import sys
@@ -38,9 +39,9 @@ def conversion(name):
     # Write the header to JAL file.
     f.write("-- -----------------------------------------------------------------------------" + newline)
     f.write("-- Title: JAL bitmap image array generated from an RGB565 bitmap file." + newline)
-    f.write("-- Author: Script by Rob Jansen, Copyright (c) 2022..2022, all rights reserved." + newline)
+    f.write("-- Author: Script by Rob Jansen, Copyright (c) 2022..2025, all rights reserved." + newline)
     f.write("-- Adapted-by:" + newline)
-    f.write("-- Compiler: 2.5r6" + newline)
+    f.write("-- Compiler: 2.5r9" + newline)
     f.write("--" + newline)
     f.write("-- This file is part of jallib (https://github.com/jallib/jallib)" + newline)
     f.write("-- Released under the ZLIB license (http://www.opensource.org/licenses/zlib-license.html)" + newline)
@@ -50,6 +51,12 @@ def conversion(name):
     f.write("-- Notes:       A way to generate the RGB565 bitmap from a png or jpg file:" + newline)
     f.write("--              Converter: https://online-converting.com/image/convert2bmp/" + newline)
     f.write("--              Choose: 16 (5:6:5 RGB Hi-Color), rows direction Top - Bottom" + newline)
+    f.write("--              If needed set a new DPI size. DPI must be an even number." + newline)
+    f.write("--" + newline)
+    if (endian == "big"):
+       f.write("--              This array uses the non-default Big Endian format!" + newline) 
+    else:
+        f.write("--              This array uses the JAL default Little Endian format." + newline) 
     f.write("--" + newline + newline)
     # Get the width and the height of the picture.
     width = (data[19] * 256) + data[18]
@@ -81,8 +88,19 @@ def conversion(name):
     index = (data[11] * 256) + data[10] + (65535 * (data[13] * 256) + data[12])
     words_per_line = 8
     while (picture_size > 0):
-        jal_word = (data[index + 1] * 256) + data[index]
-        f.write("0x" + (f'{jal_word:04x}'))
+        f.write("0x")
+        if (endian == "big"):
+            # Big Endign, MSB first.
+           jal_byte = data[index]
+           f.write(f'{jal_byte:02x}')
+           jal_byte = data[index + 1]
+           f.write(f'{jal_byte:02x}')
+        else:
+           # Normal JAL word is written with LSB first.
+           jal_byte = data[index + 1]
+           f.write(f'{jal_byte:02x}')
+           jal_byte = data[index]
+           f.write(f'{jal_byte:02x}')
         index = index + 2
         picture_size = picture_size - 1
         words_per_line = words_per_line - 1
@@ -103,11 +121,20 @@ def conversion(name):
 
 if __name__ == "__main__":
     # Main program starts here.
-    if (len(sys.argv) > 1):
+    if (len(sys.argv) == 2):
         filename = sys.argv[1]
+        endian = "little"
+    elif (len(sys.argv) == 3):
+        filename = sys.argv[1]
+        endian = sys.argv[2]
+        if (endian != "big"):
+            print("Illegal second argument")
+            sys.exit(1)
     else:
-        print("Script for converting an RGB565 BMP image to a JAL const word array")
-        print("Give the name of the bmp file as argument")
+        print("Script for converting an RGB565 BMP image to a JAL const word array.")
+        print("Give the name of the bmp file as first argument. Arguments are case sensitive.")
+        print("The word array uses Little Endian (default for JAL) which can be changed")
+        print("to Big Endian by using the optional second argument 'big'.")
         sys.exit(1)
 
     if os.path.exists(filename):
